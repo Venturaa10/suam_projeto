702272260a53b95c3cc3393ecd0c2bd2c4c87763
Adilson Lima <adilson.kirito@gmail.com>
2025-04-23 17:06:55 -0300

    Delete api directory

--- api/estatisticas.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once 'controllers/EstatisticaController.php';
-header('Content-Type: application/json');
-
-$estatisticas = EstatisticaController::obterEstatisticas();
-echo json_encode($estatisticas);
