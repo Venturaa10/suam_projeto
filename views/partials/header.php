d566340fdbf2cfc891504f08e69b3d365ab30890
Venturaa10 <joao.victor.ventura25@gmail.com>
2025-04-26 11:35:29 -0300

    - Refatorar caminhos de css.

--- views/partials/header.php
+++ views/partials/header.php
@@ -9,8 +9,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- <link rel="stylesheet" href="assets/css/form.css"> -->
     <!-- <link rel="stylesheet" href="assets/css/quiz.css"> -->
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/estatisticas.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/estatisticas.css">
     <link rel="stylesheet" href="/assets/css/home.css">
 </head>
 <body>
