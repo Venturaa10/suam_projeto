9f9550227354869e7b3f30417e562b3605ad3058
Adilson Lima <adilson.kirito@gmail.com>
2025-05-12 13:47:15 -0300

    front end home/img

--- views/home.php
+++ views/home.php
@@ -11,36 +11,90 @@
 
   <main>
     <section>
-      <h2>Bem-vindo ao nosso site!</h2>
-      <p>Este site é dedicado a ajudar pessoas leigas a entenderem os principais riscos ao navegar na internet e como se proteger de forma simples e eficaz.</p>
 
-      <h2>O que é segurança na internet?</h2>
-      <p>Podemos entender como Segurança na Internet todas os cuidados que devemos ter para proteger as coisas que fazem parte da internet como a infraestrutura, que podem ser nossos computadores e as informações, que são as mais atacadas pelos cibercriminosos.</p>
+    <div class="col-lg-4 mx-auto">
+    <h1 class="display-7 fw-bold text-body-emphasis">Bem-vindos ao nosso site!</h1>
+    </div>
+    <p class="lead mb-4">Este site é dedicado a ajudar pessoas leigas a entenderem os principais riscos ao navegar na internet e como se proteger de forma simples e eficaz.</p>
 
-      <h2>Os principais riscos da Internet</h2>
-      <p>Algumas das coisas que os cibercriminosos tentam fazer pela Internet são:</p>
-      <ul>
-        <li>Roubar informações</li>
-        <li>Corromper informações</li>   
-        <li>Atacar sistemas ou equipamentos </li> 
-        <li>Roubar identidade</li>
-        <li>Vender dados pessoais </li>  
-        <li>Roubar dinheiro</li>
-      </ul>
+    <div class="col d-flex align-items-start">
+      <img src="./assets/img/segurancadigital.png" alt="" width="45em" height="45em" > 
+      <div>
+        <h3 class="fs-2 text-body-emphasis">O que é segurança na internet?</h3>
+        <p class="lead mb-3">Podemos entender como Segurança na Internet todas os cuidados que devemos ter para proteger as coisas que fazem parte da internet como a infraestrutura, que podem ser nossos computadores e as informações, que são as mais atacadas pelos cibercriminosos.</p>
+      </div>
+    </div>
+    <br>
+    <br>
+    
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading fw-normal lh-1">Os principais riscos da Internet.</h2>
+        <p class="lead">Algumas das coisas que os cibercriminosos tentam fazer pela Internet são:</p>
+        <br>
+        <ul>
+          <li><p class="lead text-body-emphasis">Roubar informações</p></li>
+          <li><p class="lead text-body-emphasis" >Corromper informações</p></li>
+          <li><p class="lead text-body-emphasis">Atacar sistemas ou equipamentos</p></li>
+          <li><p class="lead text-body-emphasis">Roubar identidade</p></li>
+          <li><p class="lead text-body-emphasis">Vender dados pessoais</p></li>
+          <li><p class="lead text-body-emphasis">Roubar dinheiro</p></li>
+        </ul>
+      </div>
+      <div class="col-md-5">
+        <img src="./assets/img/hacking.png" alt="" width="700" height="500" >
+      </div>
+    </div>
 
-      <h2>Como evitar?</h2>
-      <p>Se alguém lida com muitas informações e possui vários equipamentos, como no caso das empresas, é melhor solicitar ajuda de profissionais que trabalham com segurança na Internet.</p>
-        
-      <p>Por outro lado, como usuário comuns, podemos adotar várias medidas preventivas, tais como manter ativos e atualizados os antivírus em nossos aparelhos que acessam à Internet, evitar fazer transações financeiras em redes abertas ou em computadores públicos e verificar os arquivos anexos das mensagens de estranhos, evitando baixá-los se não tiver certeza do seu conteúdo.</p>
-      
-      <h3>O que vão aprender aqui?</h3>
-      <ul>
-        <li><strong>Senhas Seguras:</strong> Como criar e gerenciar senhas fortes.</li>
-        <li><strong>Phishing:</strong> Como reconhecer tentativas de fraude digital.</li>
-        <li><strong>Navegação Segura:</strong> Como identificar sites confiáveis e evitar armadilhas.</li>
-        <li><strong>Privacidade:</strong> Como manter suas informações pessoais protegidas.</li>
+    <div class="col d-flex align-items-start"> 
+      <div>
+        <h3 class="fs-2 text-body-emphasis">Como evitar?</h3>
+        <p class="lead mb-3">Se alguém lida com muitas informações e possui vários equipamentos, como no caso das empresas, é melhor solicitar ajuda de profissionais que trabalham com segurança na Internet.</p>
+        <p class="lead mb-3">Por outro lado, como usuário comuns, podemos adotar várias medidas preventivas, tais como manter ativos e atualizados os antivírus em nossos aparelhos que acessam à Internet, evitar fazer transações financeiras em redes abertas ou em computadores públicos e verificar os arquivos anexos das mensagens de estranhos, evitando baixá-los se não tiver certeza do seu conteúdo.</p>
+      </div>
+    </div>
+    <br>
+    <br>
 
-      <p>Todos os tópicos são apresentados com explicações simples, vídeos ilustrativos e um quiz no final para revisar o que foi aprendido.</p>
+
+    <h3 class="fs-2 text-body-emphasis">O que vão aprender aqui?</h3>
+    <br>
+    <br>
+
+    <div class="row">
+      <div class="col-lg-3">
+        <img src="./assets/img/senhaseg.png" alt="" width="140" height="140" class="bd-placeholder-img rounded-circle">
+        <h2 class="fw-normal">Senhas Seguras</h2>
+        <p>Senhas são como chaves que protegem nossas contas e informações pessoais. Se alguém descobre sua senha, pode acessar seus e-mails, redes sociais, bancos e muito mais. Por isso, é fundamental criar senhas que sejam difíceis de adivinhar, até mesmo por pessoas que conhecem você.</p>
+        <p><a class="btn btn-secondary" href="index.php?page=senhas">Saiba mais &raquo;</a></p>
+      </div><!-- /.col-lg-3 -->
+      <div class="col-lg-3">
+        <img src="./assets/img/phishing.png" alt="" width="140" height="140" class="bd-placeholder-img rounded-circle">
+        <h2 class="fw-normal">Phishing</h2>
+        <p>Phishing é uma técnica de fraude online usada por cibercriminosos para enganar as pessoas e fazer com que elas revelem informações confidenciais, Como reconhecer tentativas de fraude digital.</p>
+        <br>
+        <br>
+        <p><a class="btn btn-secondary" href="index.php?page=phishing">Saiba mais &raquo;</a></p>
+      </div><!-- /.col-lg-3 -->
+      <div class="col-lg-3">
+        <img src="./assets/img/navsegura.png" alt="" width="140" height="140" class="bd-placeholder-img rounded-circle">
+        <h2 class="fw-normal">Navegação Segura</h2>
+        <p> Ameaças cibernéticas são perigos virtuais que podem comprometer seus dados, invadir dispositivos ou prejudicar sua privacidade, Como identificar sites confiáveis e evitar armadilhas.</p>
+        <br>
+        <br>
+        <p><a class="btn btn-secondary" href="index.php?page=navegacao">Saiba mais &raquo;</a></p>
+      </div><!-- /.col-lg-3 -->
+      <div class="col-lg-3">
+        <img src="./assets/img/privacidade.png" alt="" width="140" height="140" class="bd-placeholder-img rounded-circle">
+        <h2 class="fw-normal">Privacidade</h2>
+        <p> Privacidade online significa controlar quem tem acesso às suas informações pessoais na internet. Isso inclui seus dados em redes sociais, e-mails, apps, sites e até no seu celular.
+        Hoje em dia, com tantos golpes e vazamentos, proteger sua privacidade é essencial para evitar fraudes e prejuízos.</p>
+        <p><a class="btn btn-secondary" href="index.php?page=privacidade">Saiba mais &raquo;</a></p>
+      </div><!-- /.col-lg-3 -->
+    </div><!-- /.row -->
+    
+
+
     </section>
   </main>
 </body>
