955ef19e674b2b735d09963252bf346e88e699da
Adilson Lima <adilson.kirito@gmail.com>
2025-05-17 16:52:33 -0300

    tela privacidade

--- views/privacidade.php
+++ views/privacidade.php
@@ -1,55 +1,92 @@
+<main>
+  <section>
 
+  <div class="col-lg-4 mx-auto">
+      <h3 class="display-7 fw-bold text-body-emphasis">Como proteger sua privacidade na internet</h3>
+  </div>
+  <br>
+  <br>
 
-      <h2>Como proteger sua privacidade na internet</h2>
-
-      <h3>O que é privacidade online?</h3>
-      <p>
-        Privacidade online significa controlar quem tem acesso às suas informações pessoais na internet. Isso inclui seus dados em redes sociais, e-mails, apps, sites e até no seu celular.
+  <div class="col d-flex align-items-start"> 
+      <div>
+        <h3 class="fs-2 text-body-emphasis">O que é privacidade online?</h3>
+        <br>
+        <p class="lead mb-3">Privacidade online significa controlar quem tem acesso às suas informações pessoais na internet. Isso inclui seus dados em redes sociais, e-mails, apps, sites e até no seu celular.
         Hoje em dia, com tantos golpes e vazamentos, proteger sua privacidade é essencial para evitar fraudes e prejuízos.
-      </p>
+        </p>
+      </div>
+    </div>
+    <br>
+    <br>
 
-      <h3>Suspeita de invasão em redes sociais</h3>
-      <p>
-        Se você notar comportamentos estranhos na sua conta, como:
-      </p>
-      <ul>
-        <li>Postagens que você não fez</li>
-        <li>Mudança de e-mail ou senha</li>
-        <li>Amigos dizendo que receberam mensagens suspeitas suas</li>
-      </ul>
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading fw-normal lh-1">Suspeita de invasão em redes sociais?</h2>
+        <p class="lead">Se você notar comportamentos estranhos na sua conta, como:</p>
+        <br>
+        <ul>
+          <li><p class="lead text-body-emphasis">Postagens que você não fez</p><br></li>
+          <li><p class="lead text-body-emphasis">Mudança de e-mail ou senha</p><br></li>
+          <li><p class="lead text-body-emphasis">Amigos dizendo que receberam mensagens suspeitas suas</p><br></li>
+        </ul>
 
-      <p>
-        É bem provável que sua conta tenha sido invadida. Nesse caso: 
-        Nunca clique em links enviados por desconhecidos, mesmo que pareçam prometer ajuda. Isso pode piorar a situação.
-      </p>
+        <h5 class="lead lh-2"> É bem provável que sua conta tenha sido invadida. Nesse caso:</h5>
+        <h5 class="lead lh-2"> Nunca clique em links enviados por desconhecidos, mesmo que pareçam tentadores. Isso pode piorar a situação, entre em contato com o supporte da rede social para iniciar o processo de recuperação de conta.</h5>
+      </div>
+      <div class="col-md-5">
+        <br>
+        <img src="./assets/img/redesocial.png" alt="" width="600" height="400" >
+      </div>
+    </div>
+    <br>
+    <br>
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading fw-normal lh-1">Perdi meu celular, e agora?</h2>
+        <p class="lead">Seu celular tem acesso a:</p>
+        <br>
+        <ul>
+          <li><p class="lead text-body-emphasis">Redes sociais</p><br></li>
+          <li><p class="lead text-body-emphasis">E-mails</p><br></li>
+          <li><p class="lead text-body-emphasis">Fotos e vídeos</p><br></li>
+          <li><p class="lead text-body-emphasis">Dados bancários</p><br></li>
+        </ul>
 
-      <h3>Perdi meu celular, e agora?</h3>
-      <p>
-        Seu celular tem acesso a:
-      </p>
-      <ul>
-        <li>Redes sociais</li>
-        <li>E-mails</li>
-        <li>Fotos e vídeos</li>
-        <li>Dados bancários</li>
-      </ul>
+        <h5 class="lead lh-2"> Por isso, se você perder o aparelho, precisa agir rápido: </h5>
+        <h5 class="lead lh-2"> Isso impede que quem encontrar (ou roubar) o aparelho acesse sua vida digital.</h5>
+        <h5 class="lead lh-2"> Entre em contato com a operadora para bloquear o chip e, se possível, localize o aparelho.</h5>
+        <h5 class="lead lh-2"> Se você não conseguir recuperar o celular, altere as senhas de todas as contas que estavam logadas nele.</h5>
+        <h5 class="lead lh-2"> Além disso, ative a verificação em duas etapas para aumentar a segurança.</h5>
+      </div>
+      <div class="col-md-5">
+        <br>
+        <img src="./assets/img/perdicelular.jpg" alt="" width="600" height="400" >
+      </div>
+    </div>
+    <br> 
+    <br>
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading fw-normal lh-1">Dicas para manter sua privacidade online:</h2>
+        <br>
+        <ul>
+          <li><p class="lead text-body-emphasis">se senhas fortes e diferentes para cada conta.</p><br></li>
+          <li><p class="lead text-body-emphasis">Ative a verificação em duas etapas sempre que possível.</p><br></li>
+          <li><p class="lead text-body-emphasis">Cuidado com os apps que você instala — dê permissão apenas ao que for necessário.</p><br></li>
+          <li><p class="lead text-body-emphasis">Deslogue de contas em dispositivos públicos ou compartilhados.</p><br></li>
+          <li><p class="lead text-body-emphasis">Evite publicar dados pessoais, como endereço e telefone, em redes sociais.</p><br></li>
+        </ul>
+      </div>
+    </div>  
+      <br>
+
       
-      <p>
-        Por isso, se você perder o aparelho, precisa agir rápido:
-        Isso impede que quem encontrar (ou roubar) o aparelho acesse sua vida digital.
-      </p>
-
-      <h3>Dicas para manter sua privacidade online:</h3>
-      <ul>
-        <li>Use senhas fortes e diferentes para cada conta.</li>
-        <li>Ative a verificação em duas etapas sempre que possível.</li>
-        <li>Cuidado com os apps que você instala — dê permissão apenas ao que for necessário.</li>
-        <li>Deslogue de contas em dispositivos públicos ou compartilhados.</li>
-        <li>Evite publicar dados pessoais, como endereço e telefone, em redes sociais.</li>
-      </ul>
-
-      <h3>Assista ao vídeo:</h3>
+    <h3 class="fs-2 text-body-emphasis">Assista ao vídeo:</h3>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/c4OmEbYkfa4?si=1KOwB960qK62x1ZL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/HYISzVAKpTo?si=X-9s3Ax8YJZoVKi8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/mdmLKq3IT8k?si=luOTFy3SGPb9tKom" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      
+  </section>
+</main>
+
 
